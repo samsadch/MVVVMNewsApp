346b5d32597433ebe5caf7ac04755450d3f4089b
cvsamsad <cvsamsad@gmail.com>
2021-03-06 23:23:09 +0400

    Fragment Created

---
+++ app/src/main/java/com/samsad/mvvvmnewsapp/features/bookmarks/BookmarksFragment.kt
@@ -0,0 +1,7 @@
+package com.samsad.mvvvmnewsapp.features.bookmarks
+
+import androidx.fragment.app.Fragment
+import com.samsad.mvvvmnewsapp.R
+
+class BookmarksFragment : Fragment(R.layout.fragment_bookmark) {
+}
---
+++ app/src/main/java/com/samsad/mvvvmnewsapp/features/breakingnews/BreakingNewsFragment.kt
@@ -0,0 +1,7 @@
+package com.samsad.mvvvmnewsapp.features.breakingnews
+
+import androidx.fragment.app.Fragment
+import com.samsad.mvvvmnewsapp.R
+
+class BreakingNewsFragment : Fragment(R.layout.fragment_breaking_news) {
+}
