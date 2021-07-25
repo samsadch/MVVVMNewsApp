4665727d18d43ea487422f67e18c525f4a6b30c4
Samsad CV <samsadch@gmail.com>
2021-07-25 23:50:13 +0400

    Resource update

--- app/src/main/java/com/samsad/mvvvmnewsapp/util/Resource.kt
+++ app/src/main/java/com/samsad/mvvvmnewsapp/util/Resource.kt
@@ -4,6 +4,7 @@ sealed class Resource<T>(
     val data: T? = null,
     val error: Throwable? = null
 ) {
+    //There is no other options of subclasses available
     class Success<T>(data: T) : Resource<T>(data)
     class Loading<T>(data: T? = null) : Resource<T>(data)
     class Error<T>(throwable: Throwable, data: T? = null) : Resource<T>(data, throwable)
