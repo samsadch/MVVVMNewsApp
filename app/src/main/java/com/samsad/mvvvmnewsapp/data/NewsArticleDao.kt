cac6416a113ab0346fd74b0fa472d3d2e8ee4181
Samsad CV <samsadch@gmail.com>
2021-08-01 10:40:38 +0400

    getAllBookmarkedArticles() method added

--- app/src/main/java/com/samsad/mvvvmnewsapp/data/NewsArticleDao.kt
+++ app/src/main/java/com/samsad/mvvvmnewsapp/data/NewsArticleDao.kt
@@ -10,6 +10,9 @@ interface NewsArticleDao {
     @Query("SELECT * FROM breaking_news  INNER JOIN news_articles ON articleUrl = url")
     fun getAllBreakingNewsArticles(): Flow<List<NewsArticle>>
 
+    @Query("SELECT * FROM news_articles WHERE isBookmarked = 1")
+    fun getAllBookmarkedArticles(): Flow<List<NewsArticle>>
+
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertArticles(articles: List<NewsArticle>)
 
